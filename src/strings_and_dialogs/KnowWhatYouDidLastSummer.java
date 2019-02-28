0cb1424722eb9907156e3552bd183e423936815a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-28 12:50:42 -0800

    KnowWhatYouDidLastSummer

--- src/strings_and_dialogs/KnowWhatYouDidLastSummer.java
+++ src/strings_and_dialogs/KnowWhatYouDidLastSummer.java
@@ -1,9 +1,12 @@
 package strings_and_dialogs;
 
+import javax.swing.JOptionPane;
+
 public class KnowWhatYouDidLastSummer {
 	public static void main(String[] args) {
-		
-		
+String name=JOptionPane.showInputDialog(null,"what is your name");
+JOptionPane.showMessageDialog(null, "I know what you did last summer");
+JOptionPane.showMessageDialog(null, name+" you whent to the zoo");
 	}
 
 }
