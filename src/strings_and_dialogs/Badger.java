9ddb75267ce1b284ff997cc4fe63e69dd7aa76a8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-16 13:24:08 -0700

    myages Badger

---
+++ src/strings_and_dialogs/Badger.java
@@ -0,0 +1,21 @@
+package strings_and_dialogs;
+
+public class Badger {
+	public static void main(String[] args) {
+		for (int a = 0; a < 3; a++) {
+			
+		
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Badger");
+			
+			
+		}
+		for (int i = 0; i <2; i++) {
+			System.out.println("mushroom");
+		}}
+		for (int i = 0; i < ; i++) {
+			
+		}
+	}
+
+}
