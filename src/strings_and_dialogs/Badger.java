e2f85043c98c394c9312ea9c0e39b04589cc6fda
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-23 13:22:50 -0700

    badger

--- src/strings_and_dialogs/Badger.java
+++ src/strings_and_dialogs/Badger.java
@@ -1,21 +1,30 @@
 package strings_and_dialogs;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class Badger {
 	public static void main(String[] args) {
 		for (int a = 0; a < 3; a++) {
-			
-		
-		for (int i = 0; i < 12; i++) {
-			System.out.println("Badger");
-			
-			
+
+			for (int i = 0; i < 12; i++) {
+				System.out.print("Badger");
+			}System.out.println();
+			for (int i = 0; i < 2; i++) {
+				System.out.print("mushroom");
+			}System.out.println();
 		}
-		for (int i = 0; i <2; i++) {
-			System.out.println("mushroom");
-		}}
-		for (int i = 0; i < ; i++) {
-			
+		for (int i = 0; i < 12; i++) {
+			System.out.print("badger");
+		}
+
+		for (int i = 0; i < 1; i++) {
+			System.out.print("mush-mushroom");
+			System.out.println();
+			for (int a = 0; a < 2; a++) {
+				System.out.print("A Snake ");
+				 for (int m = 0; m < 1; m++);
+System.out.println();
+             for (int ) 
 		}
 	}
-
-}
+}}
